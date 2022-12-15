97e291cc5db21b6e03b97c499a3f05500e882c0b
Vincent <vincent@rouen-webmaster.com>
2022-12-15 11:20:15 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import './assets/base.css'
+
 createApp(App).use(router).mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,25 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "presentation",
+    component: () => import("../views/Presentation.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/programme",
+    name: "programme",
+    component: () => import("../views/Programme.vue"),
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: () => import("../views/Contact.vue"),
+  },
+  {
+    path: "/billeterie",
+    name: "billeterie",
+    component: () => import("../views/Billeterie.vue"),
   },
 ];
 
