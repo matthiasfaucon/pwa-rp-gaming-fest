e97f17352f59e47a277a2a5bcaac1f852b0ded3e
Vincent <vincent@rouen-webmaster.com>
2022-12-16 11:26:40 +0100

    change favicon

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,4 @@
 </template>
 
 <script lang="ts" setup>
-import Header from "./components/Header.vue";
-import BodyPresentation from "./components/BodyPresentation.vue";
-import Footer from "./components/Footer.vue"
 </script>
